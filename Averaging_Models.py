0a73fef67a55ea8be13c9154ca440e303e2352b4
aymaneco <59214472+aymaneco@users.noreply.github.com>
2021-02-28 21:42:12 +0100

    Add_python_code

---
+++ Averaging_Models.py
@@ -0,0 +1,18 @@
+from sklearn.base import BaseEstimator, TransformerMixin, RegressorMixin, clone
+import numpy as np
+class AveragingModels(BaseEstimator, RegressorMixin, TransformerMixin):
+  def __init__(self, X, y, models):
+      self.X = X
+      self.y = y
+      self.models = models
+  def fit(self, X, y):
+      self.models_ = [clone(x) for x in self.models]  
+        # Train cloned base models
+      for model in self.models_:
+            model.fit(self.X, self.y)
+      return self
+      
+      #Now we do the predictions for cloned models and average them
+  def predict(self, X):
+      predictions = np.column_stack([ model.predict(X) for model in self.models_ ])
+      return np.mean(predictions, axis=1)
