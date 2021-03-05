62794c8f7e86650d69cda1312668c648bfdd651a
Aymane Berradi <59214472+aymaneco@users.noreply.github.com>
2021-03-05 18:41:48 +0100

    Update Stacking_regressor.py

--- utils/Stacking_regressor.py
+++ utils/Stacking_regressor.py
@@ -3,7 +3,7 @@ from sklearn.ensemble import StackingRegressor
 from sklearn.linear_model import LinearRegression
 def Stacking_regressor(model_1, model_2, model_3):
     """
-    Stack models to enhance the perfermance.
+    Stack models to enhance the performance.
     Inputs :
         model_1 : First model ;
         model_2 : Second model ;
